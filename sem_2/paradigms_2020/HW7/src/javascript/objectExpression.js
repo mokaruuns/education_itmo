34873559b9261d6fd4f7b73cca51842ad7b549a9
M3133_Ahmetov_Marsel <284569@niuitmo.ru>
2020-04-15 10:26:16 +0300

    hw7 fixed Const.prototype

--- sem_2/paradigms_2020/HW7/src/javascript/objectExpression.js
+++ sem_2/paradigms_2020/HW7/src/javascript/objectExpression.js
@@ -1,79 +1,148 @@
-// for delay
-"use strict";
-
-function Operation() {
-    this.values = arguments;
-}
-Operation.prototype.evaluate = function () {
-    let res = [];
-    for (let value of this.values) {
-        res.push(value.evaluate.apply(value, arguments));
-    }
-    return this.calc.apply(null, res);
+const VARIABLES = ['x', 'y', 'z'];
+const BRACKET = ["(", ")"];
+const Variable = function (name) {
+    this.name = name;
 };
-Operation.prototype.toString = function () {
-    let res = "";
-    for (let value of this.values) {
-        res += value + " ";``
-    }
-    return res + this.operation;
-};
-const variables = ["x", "y", "z"];
-
-function Const(value) {
-    this.value = value;
-    this.evaluate = () => value;
-    this.toString = () => value.toString();
-}
-
-function Variable(name) {
-    this.index = variables.indexOf(name);
-}
-Variable.prototype.evaluate = function () {
-    return arguments[this.index];
+Variable.prototype.evaluate = function (...values) {
+    return values[VARIABLES.indexOf(this.name)];
 };
 Variable.prototype.toString = function () {
-    return variables[this.index];
+    return this.name;
 };
-function Add(left, right) {
-    Operation.call(this, left, right);
-    this.operation = "+";
-    this.calc = (left, right) => left + right;
+Variable.prototype.prefix = Variable.prototype.toString;
+
+const Const = function (value) {
+    this.value = value;
+};
+Const.prototype.evaluate = function () {
+    return this.value;
+};
+Const.prototype.toString = function () {
+    return this.value.toString();
+};
+Const.prototype.prefix  = Const.prototype.toString;
+
+const Operation = function (...operands) {
+    this.getOperands = () => operands;
+    this.evaluate = (...values) => {
+        const result = operands.map((operand) => operand.evaluate(...values));
+        return this.procedure(...result);
+    };
+    this.toString = function () {
+        return operands.join(" ") + " " + this.symbol;
+    }
+};
+Operation.prototype.prefix = function () {
+    return "(" + this.symbol + " " + this.getOperands().map((operand) => operand.prefix()).join(" ") + ")";
 }
-Add.prototype = Object.create(Operation.prototype);
-function Subtract(left, right) {
-    Operation.call(this, left, right);
-    this.operation = "-";
-    this.calc = (left, right) => left - right;
+const makeOp = function (procedure, symbol) {
+    const result = function (...operands) {
+        Operation.apply(this, operands);
+    };
+    result.prototype = new Operation;
+    result.prototype.procedure = procedure;
+    result.prototype.symbol = symbol;
+    return result;
+};
+const Add = makeOp((a, b) => a + b, "+");
+const Subtract = makeOp((a, b) => a - b, "-");
+const Divide = makeOp((a, b) => a / b, "/");
+const Multiply = makeOp((a, b) => a * b, "*");
+const Negate = makeOp((a) => -a, "negate");
+const Min3 = makeOp((a, b, c) => Math.min(a, b, c), "min3");
+const Max5 = makeOp((a, b, c, d, e) => Math.max(a, b, c, d, e), "max5");
+const ArcTan = makeOp(a => Math.atan(a), "atan");
+const Exp = makeOp(a => Math.exp(a), "exp");
+const OPERATIONS = {
+    "+": Add,
+    "-": Subtract,
+    "/": Divide,
+    "*": Multiply,
+    "negate": Negate,
+    "atan": ArcTan,
+    "exp": Exp
+};
+const COUNT_ARGS = {
+    "+": 2,
+    "-": 2,
+    "/": 2,
+    "*": 2,
+    "negate": 1,
+    "atan": 1,
+    "exp": 1
+};
+const Exception = (message) => {
+    const e = function (...args) {
+        this.message = message(...args);
+    };
+    e.prototype = new Error;
+    return e;
+};
+
+const UnexpectedSymbolException = Exception((expr, index) => "Expected x,y,z constant or variable : " + expr + " on position(" + index + ")");
+
+const ManyOperandsException = Exception((expr) => "Too much operands for operation in " + expr);
+
+const FewOperandException = Exception((expr) => "Not enough operands for operation in" + expr);
+
+const OperationMissedException = Exception((expr) => "Expected '+, -, *, /, atan, exp' in " + expr);
+
+const CloseBracketMissedException = Exception((expr, index) => "Expected ) in position(" + index + ") in " + expr);
+
+function isWhitespace(suspect) {
+    return suspect === " ";
 }
-Subtract.prototype = Object.create(Operation.prototype);
-function Multiply(left, right) {
-    Operation.call(this, left, right);
-    this.operation = "*";
-    this.calc = (left, right) => left * right;
+
+function Parser(expression, parse) {
+    this.token = '';
+    this.parse = parse;
+    this.expression = expression;
+    this.position = 0;
 }
-Multiply.prototype = Object.create(Operation.prototype);
-function Divide(left, right) {
-    Operation.call(this, left, right);
-    this.operation = "/";
-    this.calc = (left, right) => left / right;
-}
-Divide.prototype = Object.create(Operation.prototype);
-function Negate(value) {
-    Operation.call(this, value);
-    this.operation = "negate";
-    this.calc = (value) => -value;
-}
-Negate.prototype = Object.create(Operation.prototype);
-function Max5(...values) {
-    Operation.call(this, ...values);
-    this.operation = "max5";
-    this.calc = (...values) => Math.max(...values);
-}
-Max5.prototype = Object.create(Operation.prototype);
-function Min3(...values) {
-    Operation.call(this, ...values);
-    this.operation = "min3";
-    this.calc = (...values) => Math.min(...values);
-}
-Min3.prototype = Object.create(Operation.prototype);
+
+Parser.prototype.skipWhitespace = function () {
+    while (this.position < this.expression.length && isWhitespace(this.expression[this.position])) this.position++;
+};
+Parser.prototype.getNextToken = function () {
+    this.skipWhitespace();
+    let res = '';
+    if (BRACKET.includes(this.expression[this.position])) {
+        res = this.expression[this.position];
+        this.position++;
+    } else {
+        let i = this.position;
+        while (!BRACKET.includes(this.expression[i]) && !isWhitespace(this.expression[i]) && i < this.expression.length) i++;
+        res = this.expression.slice(this.position, i);
+        this.position = i;
+    }
+    this.skipWhitespace();
+    this.token = res;
+};
+
+const parsePrefix = function (expression) {
+    const parser = new Parser(expression, function () {
+            if (this.token === "(") {
+                this.getNextToken();
+                if (this.token in OPERATIONS) {
+                    let arguments = [];
+                    let operation = this.token;
+                    this.getNextToken();
+                    while (this.token !== ")" && this.position < expression.length) {
+                        arguments.push(this.parse());
+                        this.getNextToken();
+                    }
+                    if (this.token !== ")") throw new CloseBracketMissedException(expression, this.position);
+                    else if (COUNT_ARGS[operation] > arguments.length) throw new FewOperandException(expression);
+                    else if (COUNT_ARGS[operation] < arguments.length) throw new ManyOperandsException(expression);
+                    return new OPERATIONS[operation](...arguments);
+                } else throw new OperationMissedException(expression, this.position);
+            } else if (+this.token) return new Const(+this.token);
+            else if (VARIABLES.includes(this.token)) return new Variable(this.token);
+            else throw new UnexpectedSymbolException(expression, this.position);
+        }
+    );
+    parser.getNextToken();
+    const result = parser.parse();
+    if (parser.position < expression.length) throw new UnexpectedSymbolException(expression, parser.position);
+    return result;
+};
