866e918cbf84c6ee29c4513878d84a1cef1be11f
Marsel <marsel.ahmetov974@gmail.com>
2020-04-29 13:49:41 +0300

    solved HW10

--- sem_2/paradigms_2020/HW10/src/clojure/cljtest/functional/expression.clj
+++ sem_2/paradigms_2020/HW10/src/clojure/cljtest/functional/expression.clj
@@ -2,13 +2,13 @@
   (fn [& arguments]
     (fn [args]
       (apply f (mapv #(% args) arguments)))))
-(defn constant [value] (fn [_] value))
+(defn constant [value] (fn [x] value))
 (defn variable [name] #(get % name))
 (def add (op +))
 (def subtract (op -))
 (def negate (op -))
 (def exp (op #(Math/exp %)))
-(def ln (op (fn [x] (Math/log (Math/abs x)))))
+(def ln (op #(Math/log (Math/abs %))))
 (def multiply (op *))
 (def divide (op #(/ (double %1) %2)))
 (def operations
@@ -19,7 +19,6 @@
    '/ divide
    'exp exp
    'ln ln})
-
 (defn parse [expr]
   (cond
     (number? expr) (constant expr)
