35bcf8019355ae462405ac3839312201a179396b
Marsel <marsel.ahmetov974@gmail.com>
2020-05-20 10:12:42 +0300

    added solved problems

--- sem_2/Algorithms and Data Structures/lab3_0/C/main.cpp
+++ sem_2/Algorithms and Data Structures/lab3_0/C/main.cpp
@@ -4,16 +4,18 @@ using namespace std;
 int n;
 int d[200010];
 int dp[200010][20];
+int dm[200010][20];
 
 void binary_ups() {
     cin >> n;
     for (int i = 0; i < n - 1; i++) {
-        cin >> dp[i + 2][0];
+        cin >> dp[i + 2][0] >> dm[i + 2][0];
         d[i + 2] = d[dp[i + 2][0]] + 1;
     }
     for (int j = 1; j < 20; j++) {
         for (int i = 1; i < n + 1; i++) {
             dp[i][j] = dp[dp[i][j - 1]][j - 1];
+            dm[i][j] = min(dm[i][j - 1], dm[dp[i][j - 1]][j - 1]);
         }
     }
 }
@@ -33,17 +35,37 @@ int get_lca(int v, int u) {
     }
 }
 
+int get_min_path(int v, int u) {
+    int x = get_lca(v, u);
+    int m_v = 10e9;
+    int m_u = 10e9;
+    while (d[v] != d[x]) {
+        int l = (int) log2(d[v] - d[x]);
+        m_v = min(m_v, dm[v][l]);
+        v = dp[v][l];
+    }
+    while (d[u] != d[x]) {
+        int l = (int) log2(d[u] - d[x]);
+        m_u = min(m_u, dm[u][l]);
+        u = dp[u][l];
+    }
+    return min(m_v, m_u);
+
+}
+
 void requests() {
     int m = 0;
     cin >> m;
     for (int i = 0; i < m; i++) {
         int v, u;
         cin >> v >> u;
-        cout << get_lca(v, u) << "\n";
+        cout << get_min_path(v, u) << "\n";
     }
 }
 
 int main() {
+    freopen("minonpath.in", "r", stdin);
+    freopen("minonpath.out", "w", stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
