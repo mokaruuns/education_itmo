14ec052ce05744507f50682398282bcb630c510e
Marsel <marsel.ahmetov974@gmail.com>
2020-10-27 14:55:46 +0300

    solved hw5(3) all

--- sem_3/web/lesson5/src/main/java/ru/itmo/wp/web/page/TicTacToePage.java
+++ sem_3/web/lesson5/src/main/java/ru/itmo/wp/web/page/TicTacToePage.java
@@ -11,6 +11,7 @@ public class TicTacToePage {
     public static class State {
 
         private int size;
+        private int countEmpty;
         private String phase;
         private String[][] cells;
         private boolean crossesMove;
@@ -19,6 +20,7 @@ public class TicTacToePage {
             this.cells = new String[size][size];
             this.phase = "null";
             this.size = size;
+            this.countEmpty = size * size;
             this.crossesMove = true;
 //            for (int i = 0; i < size; i++) {
 //                for (int j = 0; j < size; j++) {
@@ -48,9 +50,10 @@ public class TicTacToePage {
     State state;
 
     private void action(HttpServletRequest request, Map<String, Object> view) {
-        System.out.println(view.get("state"));
         if (request.getSession().getAttribute("state") == null) {
             newGame(request, view);
+        } else {
+            view.put("state", request.getSession().getAttribute("state"));
         }
     }
 
@@ -73,10 +76,39 @@ public class TicTacToePage {
         }
         int x = req.charAt(req.length() - 2) - '0';
         int y = req.charAt(req.length() - 1) - '0';
-        System.out.println(x);
-        state.cells[x][y] = state.crossesMove ? "X" : "O";
-        state.crossesMove = !state.crossesMove;
+        state.phase = makeMove(x, y, state);
         view.put("state", state);
-//        checkEndGame(state.cells, state.size, x, y);
+    }
+
+    private String makeMove(int x, int y, State state) {
+        if (!state.phase.equals("RUNNING") || state.cells[x][y]!= null){
+            return state.phase;
+        }
+        state.cells[x][y] = state.crossesMove ? "X" : "O";
+        state.countEmpty--;
+        if (countElements(x, y, 0, 1, state) + countElements(x, y, 0, -1, state) + 1 >= state.size // горизонталь
+                || countElements(x, y, 1, 0, state) + countElements(x, y, -1, 0, state) + 1 >= state.size // вертикаль
+                || countElements(x, y, 1, 1, state) + countElements(x, y, -1, -1, state) + 1 >= state.size // побочная диагональ
+                || countElements(x, y, 1, -1, state) + countElements(x, y, -1, 1, state) + 1 >= state.size // главная диагональ)
+        ) {
+            return "WON_"+ (state.crossesMove?"X":"O");
+        }
+        if (state.countEmpty <= 0) {
+            return "DRAW";
+        }
+
+        state.crossesMove = !state.crossesMove;
+        return "RUNNING";
+    }
+    private int countElements(int x, int y, int i, int j, State state) {
+        x += i;
+        y += j;
+        int countElement = 0;
+        while (x >= 0 && x < state.size && y >= 0 && y < state.size && state.cells[x][y]!= null && state.cells[x][y].equals(state.crossesMove? "X" : "O")) {
+            countElement++;
+            x += i;
+            y += j;
+        }
+        return countElement;
     }
 }
