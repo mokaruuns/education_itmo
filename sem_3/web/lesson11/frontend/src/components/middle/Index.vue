12e6ca49cd4c1d1328f2f68b2c26189676800206
Marcel <marsel.ahmetov974@gmail.com>
2020-12-18 18:28:14 +0300

    solved HW11 1-2

---
+++ sem_3/web/lesson11/frontend/src/components/middle/Index.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <div>
+    <article v-for="post in posts" :key="post.id">
+      <div class="title"><a href="#">{{ post.title }}</a></div>
+      <div class="information">By {{ post.user.login }}, {{ post.creationTime }}</div>
+      <div class="body">{{ post.text }}</div>
+      <div class="footer">
+        <div class="left">
+          <img src="../../assets/img/voteup.png" title="Vote Up" alt="Vote Up"/>
+          <span class="positive-score">+173</span>
+          <img src="../../assets/img/votedown.png" title="Vote Down" alt="Vote Down"/>
+        </div>
+        <div class="right">
+          <img src="../../assets/img/date_16x16.png" title="Publish Time" alt="Publish Time"/>
+          {{ post.creationTime }}
+          <img src="../../assets/img/comments_16x16.png" title="Comments" alt="Comments"/>
+          <a href="#">0</a>
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  props: ["posts"]
+}
+</script>
+
+<style scoped>
+
+</style>
